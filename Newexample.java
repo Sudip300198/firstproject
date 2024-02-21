5456311c7584bd2efd11fe22ca245588e0588ebd
Sudip300198 <das808865@gmail.com>
2024-02-21 11:37:13 +0530

    this is new ccommit

---
+++ Newexample.java
@@ -0,0 +1,21 @@
+public class Newexample {
+    public static void main(String[] args) {
+        //Primitive data types
+        int integerValue = 42;
+        double doubleValue = 3.14;
+        char characterValue = 'A';
+        boolean booleanValue = true;
+
+        //Displaying primitive data
+        System.out.println("Integer Value:" + integerValue);
+        System.out.println("Double Value:" + doubleValue);
+        System.out.println("CharacterValue:" + characterValue );
+        System.out.println("BooleanValue:" + booleanValue);
+       
+        
+        //String data type
+        String stringValue = "Hello Java";
+
+        System.out.println("Strirng value:" + stringValue);
+    }
+}
---
+++ People.java
@@ -0,0 +1,32 @@
+public class People{
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+        int sum = num1 +  num2;
+        int difference = num1 - num2;
+        int product = num1 * num2;
+        int quotient = num1 / num2;
+        
+
+        System.out.println("Num1: " + num1);
+        System.out.println("Num2: " + num2);
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+
+
+        double decimalNum = 3.14;
+        float floatNum = 2.5f;
+        char character ='A';
+        boolean isJavaFun = true;
+
+
+        System.out.println("Decimal Number: " + decimalNum);
+        System.out.println("Float Number " + floatNum);
+        System.out.println("Character " + character);
+        System.out.println("Is Java Fun " + isJavaFun);
+
+
+    }
+}
