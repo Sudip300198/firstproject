ffc78a2303f8ce3474c9734bc9f29615771cc3d6
Sudip300198 <das808865@gmail.com>
2024-02-22 09:54:21 +0530

    this is java script

---
+++ variable.js
@@ -0,0 +1,10 @@
+let num = 10;
+var num1 = 10;
+const num2 = 10;
+num = 15;
+
+console.log("The number changed into: "       + num)
+console.log("The second value is: " +    num1)
+console.log(num2)
+
+console.table("This is how we can creat as table: " + num, num1, num2);
